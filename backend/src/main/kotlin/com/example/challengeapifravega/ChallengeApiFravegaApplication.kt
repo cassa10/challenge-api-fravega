5120564cda379fcc8d67d8ff28f1850825363dee
cassa10 <cassanojoseluis@gmail.com>
2020-11-27 18:50:46 -0300

    Start up

---
+++ backend/src/main/kotlin/com/example/challengeapifravega/ChallengeApiFravegaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.challengeapifravega
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChallengeApiFravegaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChallengeApiFravegaApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/example/challengeapifravega/ChallengeApiFravegaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.challengeapifravega
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChallengeApiFravegaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
